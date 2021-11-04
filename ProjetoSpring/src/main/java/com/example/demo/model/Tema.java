409ab6f6a907e86ff7c7ecaa2f8a3dcc3adf3cbf
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-11-04 10:04:25 -0300

    Inicio do projeto com Spring

---
+++ ProjetoSpring/src/main/java/com/example/demo/model/Tema.java
@@ -0,0 +1,52 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String tipo;
+	
+	@NotNull
+	private String descricao;
+	
+	@NotNull
+	private String segmento;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getSegmento() {
+		return segmento;
+	}
+	public void setSegmento(String segmento) {
+		this.segmento = segmento;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
