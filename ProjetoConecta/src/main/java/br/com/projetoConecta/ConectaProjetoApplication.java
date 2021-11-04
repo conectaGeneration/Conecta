39208c4fa3f03c899fc2f5234339bff6f49b3ca9
Gabriel Balbino <gbalbino098@gmail.com>
2021-11-04 10:13:33 -0300

    Projeto Conecta

---
+++ ProjetoConecta/src/main/java/br/com/projetoConecta/ConectaProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoConecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaProjetoApplication.class, args);
+	}
+
+}
