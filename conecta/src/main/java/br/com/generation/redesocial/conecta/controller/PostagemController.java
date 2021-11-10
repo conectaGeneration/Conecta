6ad5dd8449a74a7f3750c6f09abccc06ccec3e15
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-11-10 13:52:20 -0300

    Task 6 - Postagem model, controller e repository

--- conecta/src/main/java/br/com/generation/redesocial/conecta/controller/PostagemController.java
+++ conecta/src/main/java/br/com/generation/redesocial/conecta/controller/PostagemController.java
@@ -1,6 +1,7 @@
 package br.com.generation.redesocial.conecta.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,44 +14,41 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.generation.redesocial.conecta.model.Tema;
-import br.com.generation.redesocial.conecta.repository.TemaRepository;
+
+import br.com.generation.redesocial.conecta.model.Postagem;
+import br.com.generation.redesocial.conecta.repository.PostagemRepository;
 
 @RestController
-@RequestMapping("/tema")
-@CrossOrigin (origins = "*", allowedHeaders = "*")
-public class TemaController {
-
+@RequestMapping("/postagem")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
 	@Autowired
-	private TemaRepository repository;
+	private PostagemRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<Tema>> getAll() {
+	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Tema> getById (@PathVariable long id) {
+	public ResponseEntity<Postagem> getById(@PathVariable long id){
 		return repository.findById(id)
 				.map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
 	}
-	
-	@GetMapping("/segmento/{segmento}")
-	public ResponseEntity<List<Tema>> getBySegmento (@PathVariable String segmento) {
-		return ResponseEntity.ok(repository.findAllBySegmentoContainingIgnoreCase(segmento));
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo (@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
 	@PostMapping
-	public ResponseEntity<Tema> post (@RequestBody Tema tema){
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	public ResponseEntity<Postagem> PostPostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
 	}
-	
 	@PutMapping
-	public ResponseEntity<Tema> put (@RequestBody Tema tema){
-		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
+	public ResponseEntity<Postagem> PutPostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
-	
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
